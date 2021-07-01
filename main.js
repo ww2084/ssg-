5360c74ac7aed7028ec084867881a7806fd04851
ww2084 <84954684+ww2084@users.noreply.github.com>
2021-07-01 11:11:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-// main.js
 $(function(){
   
-  var titleData = [
-    {
-      imgUrl: 'images/14.png',
-      bgUrl: 'images/ssg_wordmark1.png'
-    },
-    {
-      imgUrl: 'images/0.png',
-      bgUrl: 'images/ssg_wordmark3.png'
-    },
-    {
-      imgUrl: 'images/35.png',
-      bgUrl: 'images/ssg_wordmark2.png'
-    },
-  ];
-
+    var titleData = [
+      {
+        imgUrl: 'images/14.png',
+        bgUrl: 'images/ssg_wordmark1.png'
+      },
+      {
+        imgUrl: 'images/0.png',
+        bgUrl: 'images/ssg_wordmark3.png'
+      },
+      {
+        imgUrl: 'images/35.png',
+        bgUrl: 'images/ssg_wordmark2.png'
+      },
+    ];
   
-  $('.pager > ul > li').eq(0).click(function(){
     
-    $('figure img')
-      .attr('src',  titleData[0].imgUrl);
-    
-    $('#title figure').css('background-image', `url(${titleData[0].bgUrl})`);
-    console.log(titleData[0].bgUrl)
-  });
-
-  $('.pager > ul > li').eq(1).click(function(){
-    $('figure img')
-      .attr('src',  titleData[1].imgUrl);
-    $('#title figure').css('background-image', `url(${titleData[1].bgUrl})`);  
-    console.log(titleData[0].bgUrl)
-  });
-
-  $('.pager > ul > li').eq(2).click(function(){
-    $('figure img')
-      .attr('src',  titleData[2].imgUrl);
-      $('#title figure').css('background-image', `url(${titleData[2].bgUrl})`);    
-  });
-
-
-})
+    $('.pager > ul > li').eq(0).click(function(){
+      
+      $('figure img')
+        .attr('src',  titleData[0].imgUrl);
+      
+      $('#title figure').css('background-image', `url(${titleData[0].bgUrl})`);
+      console.log(titleData[0].bgUrl)
+    });
+  
+    $('.pager > ul > li').eq(1).click(function(){
+      $('figure img')
+        .attr('src',  titleData[1].imgUrl);
+      $('#title figure').css('background-image', `url(${titleData[1].bgUrl})`);  
+      console.log(titleData[0].bgUrl)
+    });
+  
+    $('.pager > ul > li').eq(2).click(function(){
+      $('figure img')
+        .attr('src',  titleData[2].imgUrl);
+        $('#title figure').css('background-image', `url(${titleData[2].bgUrl})`);    
+    });
+  
+  
+  })
